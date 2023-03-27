6b66f5716226745224abbb97df5673c25b45c61d
Scott Ladd <sladd.33@gmail.com>
2023-03-27 10:59:39 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# About
+
+This application is a combination of an Angular application and an AWS CDK application. The purpose is to show how one could create, build, deploy, and maintain an angular application in AWS via the AWS CDK with CI/CD pipeline configuration. The angular application resides in the root of the repository while the CDK app resides in the CDK folder. There are modifications both apps to make this work and I'll attempt to cover all modifications and reasoning as best I can.
+
+For the Angular application, you will need to create an additional build configuration. In my case, I called it `dev` but you could have many ex: dev, test, qa, production. These can all be supported through this example repository. You will find the build configurations in the angular.json in the root of the repository. There are currently 3 albeit `development` is your local development work. We'll mainly focus on `production` and `dev`. Each environment has its' own environment.ts file under the `src/environments/` folder. This allows you to have differing environment variables specific to the environment you're building. 
+
+For the CDK, we have created our environment/app information in the cdk.json, however; this information could be overridden with the context flag and the appropriate key value pairs passed. I won't get into that too much though. If we look at the cdk.json within the `cdk/` folder, we can note that we have a globals section which defines our base application and variables. These are variables that are not specific to any one environment. Beyond that, we have variables for both `dev` and `production`. These variables are required to deploy the project and most should be self-explanatory. There is one assumption in here. In my example, I'm using a CodeStar connection that I previously setup. You will want to setup a similar CodeStar connection within your aws account pointing to github and then paste the arn in the value there. Hopefully, you've forked this repo so you can set the owner and repo name to whatever you may be using. The owner is often the user but can also be an organization. Finally, I'm using 2 branches `dev` and `main`. You can create as many environments as you need and as many branches as fit those environments.
+
+## AwsCdkPipelineAngular
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.8.
+
+## Development server
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+
+## Code scaffolding
+
+Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+
+## Build
+
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
+
+## Running unit tests
+
+Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+
+## Running end-to-end tests
+
+Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
+
+## Further help
+
+To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
---
+++ cdk/lib/cdk-stack.ts
@@ -0,0 +1,265 @@
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+import { aws_cloudfront as cloudfront } from 'aws-cdk-lib';
+import { Artifact, Pipeline, StageOptions } from 'aws-cdk-lib/aws-codepipeline';
+import { BuildSpec, LinuxBuildImage, PipelineProject } from 'aws-cdk-lib/aws-codebuild';
+import { ARecord, HostedZone, RecordTarget } from 'aws-cdk-lib/aws-route53';
+import { DnsValidatedCertificate } from 'aws-cdk-lib/aws-certificatemanager';
+import { CloudFrontWebDistribution, ViewerCertificate } from 'aws-cdk-lib/aws-cloudfront';
+import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets';
+import { CodeBuildStep, CodePipeline, CodePipelineFileSet } from 'aws-cdk-lib/pipelines';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+
+export class CdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Look up environment specific configuration. Default to dev.
+    const envName: string = this.node.tryGetContext('ENV_NAME') || 'development';
+    console.log(`Environment: ${envName}`);
+
+    // Create a CDKContext object that follows our structure. The values for this can be found in the cdk.json file.
+    const envConfig = scope.node.tryGetContext(envName);
+    const globalConfig = scope.node.tryGetContext('globals');
+    const context: CDKContext = { ...globalConfig, ...envConfig };
+    console.log(context);
+
+    // Define s3 bucket.
+    const appBucket = new s3.Bucket(this, `${context.appName}-bucket`, {
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'index.html',
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // This flags S3 bucket to be destroyed when you want to tear down your infrastructure.
+      autoDeleteObjects: true
+    });
+
+    // Configure domain and sub-domain urls.
+    const domainName = context.domain;
+    const subDomainName = `${context.appName}.` + domainName; // We're deploying off a subdomain, but you could remove this property and set all references to it to domainName if you're doing top level.
+    console.log(`domainName: ${domainName}`);
+    console.log(`subDomainName: ${subDomainName}`);
+
+    // Look up hosted zone.
+    const hostedZone = HostedZone.fromLookup(this, domainName, {
+      domainName: domainName,
+      privateZone: false,
+    });
+
+    // Define SSL Certificate (only use us-east-1). TODO: Replace with Certificate
+    const frontendCertificate = new DnsValidatedCertificate(this, `${context.appName}-certificate`, {
+      domainName: subDomainName,
+      hostedZone: hostedZone,
+      region: 'us-east-1',
+    });
+
+    const viewerCertificate = ViewerCertificate.fromAcmCertificate(frontendCertificate, { // .fromIamCertificate('MYIAMROLEIDENTIFIER', {
+      aliases: [subDomainName],
+    });
+
+    // Define CloudFront Distribution with error responses.
+    const accessDeniedErrorResponse: cloudfront.CfnDistribution.CustomErrorResponseProperty = {
+      errorCode: 403,
+      errorCachingMinTtl: 30,
+      responseCode: 200,
+      responsePagePath: '/index.html',
+    };
+    const notFoundErrorResponse: cloudfront.CfnDistribution.CustomErrorResponseProperty = {
+      errorCode: 404,
+      errorCachingMinTtl: 30,
+      responseCode: 200,
+      responsePagePath: '/index.html',
+    };
+    const distribution = new CloudFrontWebDistribution(this, `${context.appName}-web-distribution`, {
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: appBucket,
+          },
+          behaviors: [{ isDefaultBehavior: true }],
+        },
+      ],
+      viewerCertificate: viewerCertificate,
+      errorConfigurations: [
+        accessDeniedErrorResponse,
+        notFoundErrorResponse
+      ]
+    });
+
+    // Define A Record in Route53 for sub-domain.
+    new ARecord(this, 'ARecord', {
+      recordName: subDomainName,
+      zone: hostedZone,
+      target: RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
+    });
+
+    // Output the url of the website.
+    new cdk.CfnOutput(this, 'URL', {
+      description: 'The url of the website',
+      value: appBucket.bucketWebsiteUrl + "\n" + subDomainName
+    });
+
+    // AWS CodeBuild artifacts
+    const outputSources = new Artifact(`${context.appName}-ui-source-artifact`);
+    const outputArtifact = new Artifact(`${context.appName}-ui-app-deploy-artifact`);
+
+    // Define our pipeline and stages.
+    const pipeline = new Pipeline(this, `${context.appName}UIPipeline`, {
+      restartExecutionOnUpdate: true
+    });
+
+    // Create Source Stage.
+    let sourceStage: StageOptions = {
+      stageName: 'Source',
+      actions: [
+        new cdk.aws_codepipeline_actions.CodeStarConnectionsSourceAction({
+          actionName: 'Checkout',
+          branch: context.repo.branch,
+          connectionArn: context.codeStarConnectionArn,
+          output: outputSources,
+          owner: context.repo.owner,
+          repo: context.repo.name
+        })
+      ]
+    };
+    pipeline.addStage(sourceStage);
+
+    // Create Build Stage.
+    let buildStage: StageOptions = {
+      stageName: 'BuildApp', // Cannot be `Build` as that name is reserved during the synth step.
+      actions: [
+        new cdk.aws_codepipeline_actions.CodeBuildAction({
+          actionName: `${context.appName}-build-action`,
+          project: new PipelineProject(this, `${context.appName}-build-project-id`, {
+            projectName: `${context.appName}-build-project`,
+            environment: {
+              buildImage: LinuxBuildImage.AMAZON_LINUX_2_4
+            },
+            buildSpec: BuildSpec.fromObject({
+              version: 0.2,
+              phases: {
+                install: {
+                  'runtime-versions': {
+                    nodejs: 16
+                  },
+                  commands: [
+                    'npm install -g @angular/cli@14.2.8', // I'm using 14.2.8, but this can be changed to match your needs.
+                    'npm install'
+                  ]
+                },
+                build: {
+                  commands: [
+                    'echo Build started',
+                    'ng build --configuration=' + context.environment // This allows you to pass the environment through so your builds align with your pipelines/envs.
+                  ]
+                },
+                post_build: {
+                  commands: [
+                    'echo Build Complete'
+                  ]
+                }
+              },
+              artifacts: {
+                'files': ['**/*'],
+                'base-directory': context.baseDir
+              }
+            })
+          }),
+          input: outputSources,
+          outputs: [outputArtifact]
+        })
+      ]
+    };
+    pipeline.addStage(buildStage);
+
+    // Create Approval Stage for Production only.
+    if (context.isProd) {
+      let approveStage: StageOptions = {
+        stageName: 'Approve',
+        actions: [
+          new cdk.aws_codepipeline_actions.ManualApprovalAction({
+            actionName: "ApprovalAction"
+          })
+        ]
+      }
+      pipeline.addStage(approveStage);
+    }
+
+    // Create CloudFront Distribution Invalidation Project.
+    const invalidateBuildProject = new PipelineProject(this, `${context.appName}-invalidate-project-id`, {
+      environmentVariables: {
+        CLOUDFRONT_ID: { value: distribution.distributionId }
+      },
+      buildSpec: BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          build: {
+            commands: [
+              'aws cloudfront create-invalidation --distribution-id ${CLOUDFRONT_ID} --paths "/*"'
+            ]
+          }
+        }
+      })
+    });
+
+    // Add CloudFront invalidation permissions to the project. If you don't create invalidations, your site may be cached for a lengthy period of time.
+    const distributionArn = `arn:aws:cloudfront::${this.account}:distribution/${distribution.distributionId}`
+    invalidateBuildProject.addToRolePolicy(new PolicyStatement({
+      resources: [distributionArn],
+      actions: [
+        'cloudfront:CreateInvalidation',
+      ],
+    }));
+
+    // Create Deployment Stage.
+    let deployStage: StageOptions = {
+      stageName: "DeployApp",
+      actions: [
+        new cdk.aws_codepipeline_actions.S3DeployAction({
+          actionName: "DeployApplication",
+          input: outputArtifact,
+          bucket: appBucket,
+          runOrder: 1
+        }),
+        new cdk.aws_codepipeline_actions.CodeBuildAction({
+          actionName: "InvalidateCache",
+          project: invalidateBuildProject,
+          input: outputArtifact,
+          runOrder: 2
+        })
+      ]
+    };
+    pipeline.addStage(deployStage);
+
+    // Creates synth step that allows for your pipeline to self mutate when infrastructure changes occur.
+    const synthPipeline = new CodePipeline(this, `${context.appName}-synth-pipeline-id`, {
+      codePipeline: pipeline,
+      synth: new CodeBuildStep('Synth', {
+        input: CodePipelineFileSet.fromArtifact(outputSources),
+        installCommands: ["npm install -g aws-cdk"],
+        commands: [
+          'cd cdk',
+          'npm ci',
+          'npm run build',
+          'npx cdk synth --context ENV_NAME=' + context.environment
+        ],
+        primaryOutputDirectory: "cdk/cdk.out"
+      })
+    });
+  }
+}
+
+export type CDKContext = {
+  appName: string;
+  region: string;
+  environment: string;
+  isProd: boolean;
+  domain: string;
+  baseDir: string;
+  codeStarConnectionArn: string;
+  repo: {
+    owner: string,
+    name: string,
+    branch: string
+  }
+}
